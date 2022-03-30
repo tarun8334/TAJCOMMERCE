f48e1a00e682ab02b8404ff50e9c503196cd433a
Tarun jindal <82716184+tarun8334@users.noreply.github.com>
2022-03-30 17:35:28 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className = 'footer'>
+        <p>Designed and developed by </p>
+<hr/>     
+<p>Tarun</p>
+    </div>
+  );
+}
+
+export default Footer;
