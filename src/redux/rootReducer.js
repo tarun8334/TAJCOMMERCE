7eacf78c747d843347b7faff7ce26b6c1b77c4bf
Tarun jindal <82716184+tarun8334@users.noreply.github.com>
2022-07-25 23:51:44 +0530

    Delete src directory

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import {cartReducer} from "./cartReducer";
-
-const rootReducer = combineReducers({
-cartReducer : cartReducer,
-})
-
-export default rootReducer
